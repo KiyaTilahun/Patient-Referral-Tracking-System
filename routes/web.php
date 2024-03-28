67e73291fae0eaf27dc2acc5c871b837b4b09044
KiyaTilahun <kiyatilahun0@gmail.com>
2024-03-28 11:30:57 +0300

    login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', function(){
+
+    return redirect('/login');
+});
 
 Route::get('dashboard', Dashboard::class)
     ->middleware(['auth', 'verified'])
